66ae5381d5e478498f8014b6c4e2ae7c85139599
Santosh Parse <santosh.parse@gmail.com>
2022-12-31 14:59:05 +0530

    CC practical 02B

---
+++ cloud_computing/prac02B/udpClientFact.java
@@ -0,0 +1,28 @@
+package prac02B;
+
+import java.net.*;
+import java.io.*;
+
+public class udpClientFact {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket ds = new DatagramSocket(1000);
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter a number : ");
+            String num = br.readLine();
+            byte b[] = new byte[1024];
+            b = num.getBytes();
+            DatagramPacket dp = new DatagramPacket(b, b.length, InetAddress.getLocalHost(), 2000);
+            ds.send(dp);
+            byte b1[] = new byte[1024];
+            DatagramPacket dp1 = new DatagramPacket(b1, b1.length);
+            ds.receive(dp1);
+            String str = new String(dp1.getData(), 0, dp1.getLength());
+            System.out.println(str);
+
+            ds.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ cloud_computing/prac02B/udpServerFact.java
@@ -0,0 +1,33 @@
+package prac02B;
+
+import java.net.*;
+
+public class udpServerFact {
+
+    public static void main(String[] args) {
+        try {
+            DatagramSocket ds = new DatagramSocket(2000);
+            byte b[] = new byte[1024];
+            DatagramPacket dp = new DatagramPacket(b, b.length);
+            ds.receive(dp);
+            String str = new String(dp.getData(), 0, dp.getLength());
+            System.out.println(str);
+            int a = Integer.parseInt(str);
+            int f = 1, i;
+            String s = new String();
+            for (i = 1; i <= a; i++) {
+                f = f * i;
+            }
+            s = Integer.toString(f);
+            String str1 = "The Factorial of " + str + " is : " + s;
+            byte b1[] = new byte[1024];
+            b1 = str1.getBytes();
+            DatagramPacket dp1 = new DatagramPacket(b1, b1.length, InetAddress.getLocalHost(), 1000);
+            ds.send(dp1);
+
+            ds.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
