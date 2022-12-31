3b6ddd8d584a6c4988dcc1bd8094c038c316f5c7
Santosh Parse <santosh.parse@gmail.com>
2022-12-31 14:36:40 +0530

    CC practial 2A

---
+++ cloud_computing/prac02A/udpClientEO.java
@@ -0,0 +1,29 @@
+package prac02A;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.*;
+
+public class udpClientEO {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket ds = new DatagramSocket(1000);
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter a number : ");
+            String num = br.readLine();
+            byte b[] = new byte[1024];
+            b = num.getBytes();
+            DatagramPacket dp = new DatagramPacket(b, b.length, InetAddress.getLocalHost(), 2000);
+            ds.send(dp);
+            byte b1[] = new byte[1024];
+            DatagramPacket dp1 = new DatagramPacket(b1, b1.length);
+            ds.receive(dp1);
+            String str = new String(dp1.getData(), 0, dp1.getLength());
+            System.out.println(str);
+
+            ds.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
