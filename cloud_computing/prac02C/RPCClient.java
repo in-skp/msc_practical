32a661a090dbf0061119ad6b3cdc1d0bc978b377
Santosh Parse <santosh.parse@gmail.com>
2022-12-31 20:58:17 +0530

    CC practical 02C

---
+++ cloud_computing/prac02C/RPCClient.java
@@ -0,0 +1,36 @@
+package prac02C;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class RPCClient {
+    RPCClient() {
+        try {
+            InetAddress ia = InetAddress.getLocalHost();
+            DatagramSocket ds = new DatagramSocket();
+            DatagramSocket ds1 = new DatagramSocket(1300);
+            System.out.println("\nRPC Client\n");
+            System.out.println("Enter method name and parameter like add 3 4\n");
+            while (true) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                String str = br.readLine();
+                byte b[] = str.getBytes();
+                DatagramPacket dp = new DatagramPacket(b, b.length, ia, 1200);
+                ds.send(dp);
+                dp = new DatagramPacket(b, b.length);
+                ds1.receive(dp);
+                String s = new String(dp.getData(), 0, dp.getLength());
+                System.out.println("\nResult = " + s + "\n");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new RPCClient();
+    }
+}
