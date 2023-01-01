60e2534bac49d321d974a3e70f523d65d0fd2c9f
Santosh Parse <santosh.parse@gmail.com>
2023-01-01 12:15:45 +0530

    CC practical 03A

---
+++ cloud_computing/prac03A/BroadcastServer.java
@@ -0,0 +1,30 @@
+package prac03A;
+
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class BroadcastServer {
+    public static final int PORT = 1234;
+
+    public static void main(String args[]) throws Exception {
+        MulticastSocket socket;
+        DatagramPacket packet;
+        InetAddress address;
+        // set the multicast address to your local subnet
+        address = InetAddress.getByName("239.1.2.3");
+        socket = new MulticastSocket();
+        // join a Multicast group and send the group messages
+        socket.joinGroup(address);
+        byte[] data = null;
+        for (;;) {
+            Thread.sleep(10000);
+            System.out.println("Sending ");
+            String str = ("This is Pushpa Calling....");
+            data = str.getBytes();
+            packet = new DatagramPacket(data, str.length(), address, PORT);
+            // Sends the packet
+            socket.send(packet);
+        }
+    }
+}
